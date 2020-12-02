45222b85b132eb5e6d78dfb1afe27616e25b4662
Eugene <eugene.prout@warwick.ac.uk>
2020-12-02 11:19:21 +0000

    day 2: removed some lines

--- 2/sol.py
+++ 2/sol.py
@@ -1,11 +1,10 @@
 
 def part1():
     with open("input.txt", 'r') as f:
-        content = [x.strip() for x in f.readlines()]
+        content = [x.strip().split() for x in f.readlines()]
 
     counter = 0
-    for l in content:
-        line = l.split()
+    for line in content:
         lower, upper = [int(x) for x in line[0].split("-")]
         if (line[2].count(line[1][0]) in range(lower, upper+1)):
             counter += 1
@@ -14,11 +13,10 @@ def part1():
 
 def part2():
     with open("input.txt", 'r') as f:
-        content = [x.strip() for x in f.readlines()]
+        content = [x.strip().split() for x in f.readlines()]
 
     counter = 0
-    for l in content:
-        line = l.split()
+    for line in content:
         positions = [int(x)-1 for x in line[0].split("-")]
         
         if sum([line[1][0] == line[2][index] for index in positions]) == 1:
