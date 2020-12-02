c8c9435f1dac73c74731c0c63595d38bba9d678a
Eugene <eugene.prout@warwick.ac.uk>
2020-12-02 11:43:08 +0000

    Day 2: actual one line

--- 2/sol.py
+++ 2/sol.py
@@ -1,8 +1,6 @@
 
 def part1():
-    with open("input.txt", 'r') as f:
-        print(len(list(filter(lambda l: (int(l[0].split("-")[0]) <= l[2].count(l[1][0]) <= int(l[0].split("-")[1])), [x.strip().split() for x in f.readlines()]))))
+    with open("input.txt", 'r') as f: print(len(list(filter(lambda l: (int(l[0].split("-")[0]) <= l[2].count(l[1][0]) <= int(l[0].split("-")[1])), [x.strip().split() for x in f.readlines()]))))
 
 def part2():
-    with open("input.txt", 'r') as f:
-        print(len(list(filter(lambda l: sum([l[1][0] == l[2][index] for index in [int(x)-1 for x in l[0].split("-")]]) == 1, [x.strip().split() for x in f.readlines()]))))
+    with open("input.txt", 'r') as f: print(len(list(filter(lambda l: sum([l[1][0] == l[2][index] for index in [int(x)-1 for x in l[0].split("-")]]) == 1, [x.strip().split() for x in f.readlines()]))))
